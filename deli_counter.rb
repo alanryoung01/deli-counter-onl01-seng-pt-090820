c97091dcaaaa72290d1a2a03ce4ba5f2e5e6c29a
Alan Young <alanryoung01@gmail.com>
2020-09-15 10:07:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 katz_deli = []
-another_deli = ["The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6.
-Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey"]
 
 def line(katz_deli)
 	if katz_deli.empty?
 		puts "The line is currently empty."
-	else
+	elsif line.length < 4
 		puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
+  else "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]} 4. #{katz_deli[3]} 5. #{katz_deli[4]} 6. #{katz_deli[5]} 7. #{katz_deli[6]} 8. #{katz_deli[7]} 9. #{katz_deli[8]} 10. #{katz_deli[9]}"
+    puts 
 	end
 end
 
