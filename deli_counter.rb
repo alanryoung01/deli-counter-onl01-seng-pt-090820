8f57b181cb147b56c692704e056d7cbbcc6a2e3c
Alan Young <alanryoung01@gmail.com>
2020-09-15 09:46:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,14 @@
+katz_deli = []
+
+def line(katz_deli)
+	if katz_deli.empty?
+		puts "The line is currently empty."
+	else
+		puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
+	end
+end
+
+
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   number = katz_deli.index(name) + 1
