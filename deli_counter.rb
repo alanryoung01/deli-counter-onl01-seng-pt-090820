cd74ca621ad16dda500d75e11db6631cdf1e0123
Alan Young <alanryoung01@gmail.com>
2020-09-15 09:49:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,13 @@ def take_a_number(katz_deli, name)
   number = katz_deli.index(name) + 1
   puts "Welcome, #{name}. You are number #{number} in line."
 end
+
+def now_serving(katz_deli)
+	if katz_deli.empty?
+		puts "There is nobody waiting to be served!"
+	else
+		serving = katz_deli[0]
+		puts "Currently serving #{serving}."
+		katz_deli.shift
+	end
+end
